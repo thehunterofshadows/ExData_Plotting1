ccdd0a34b730445d6f92ee94646906ca7a3174d1
Justin <thehunterofshadows@gmail.com>
2019-04-27 10:47:28 -0500

    First plot done

--- plot1.R
+++ plot1.R
@@ -1,12 +1,13 @@
 #code for plot 1
 #***Before submission, add descriptoins, and remove # from return data so it works
 mySetup<-function(){
+  #loads the librarys used.
   library(data.table)
   library(lubridate)
-  library(dplyr)
 }
 
 dlFile<-function(){
+  #Downloads the zip file to use.
   temp <- tempfile()
   download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
   unzip(temp)
@@ -14,19 +15,32 @@ dlFile<-function(){
 }
 
 loadFile<-function(){
+  #loads the file
   mydata <<- fread("household_power_consumption.txt",sep=";",header=TRUE)
 }
 
 cleanData<-function(){
-  #Here we will remove all but the dates we need
-  mydata$Date <<- dmy(my$Date)
+  #Cleans up the data in prep for the plot
+  mydata$Date <<- dmy(mydata$Date)
   mydata<<-subset(mydata, Date=="2007-02-01" | Date == "2007-02-02")
   mydata$Global_active_power<<-as.numeric(mydata$Global_active_power)
 }
 
+plot1png<-function(){
+  #exports the plot
+  png("plot1.png")
+  hist(returnData()$Global_active_power, col="Red", xlab="Global Active Power (kilowatts)", main="Global Active Power" )
+  dev.off()
+}
+
 returnData<-function(){
-  #mySetup()
-  #dfFile()
-  loadFile()
+  #returns the if needed for testing purposes
   return(mydata)
 }
+
+#calls the functions to do the work.
+mySetup()
+dlFile()
+loadFile()
+cleanData()
+plot1png()
